b4d53166f6f0be9f40470542c4080c434d3f85dd
Андрей Стригалев <siheshteger@gmail.com>
2022-09-11 11:29:42 +0300

    Добавьте файлы проекта.

---
+++ Lesson_C++/Calc.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+
+class Calculate
+{
+public:
+    void Start(double inputNumber)
+    {
+        int index = 1;
+        for (float x = start; x <= stop; x += step)
+        {
+            double sinus = sin(x);
+            double square = sqrt(sinus - 0.5);
+            double del = inputNumber - square;
+            double y = 2 / del;
+
+            CreateTable(x, y, index);
+            index++;
+        }
+        std::cout << border << std::endl;
+
+    };
+
+    bool CheckStringOnDigit(std::string str)
+    {
+        bool isNegative = false, tough = false;
+        int index = 0;
+
+        if (str[0] == '-')
+        {
+            index++;
+            isNegative = true;
+        }
+
+        while (index != str.length())
+        {
+            if (str[index] == '.')
+                if (!tough)
+                    tough = true;
+                else
+                    return false;
+            else if (!isdigit(str[index]))
+                return false;
+
+            index++;
+        }
+
+        return true;
+    };
+
+    bool CheckDigitOnExpress(double digit)
+    {
+        if (digit <= -1000000)
+            return false;
+        else if (digit >= 1000000)
+            return false;
+
+        return true;
+    };
+
+private:
+    const std::string border = "--------------------------------------";
+    const float start = -2, stop = 7, step = 0.5;
+
+    void CreateTable(double x, double y, int index)
+    {
+        std::cout << border << std::endl;
+        std::cout << "|  " << std::setw(2) << index << std::setw(5) << "|  " << std::setw(4) << std::setprecision(1) << x << "  |  ";
+        std::cout << std::fixed << std::setw(16) << std::setprecision(6) << y << std::setw(3) << "|" << std::endl;
+    }
+};
+
---
+++ Lesson_C++/Lesson_C++.cpp
@@ -0,0 +1,32 @@
+﻿#include "Calc.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Calculate calc;
+
+    std::string str;
+    std::cout << "Введите параметр А ";
+    std::cin >> str;
+    
+    if (!calc.CheckStringOnDigit(str))
+    {
+        std::cout << "Параметр А не является числом." << std::endl;
+        return 0;
+    }
+
+    double inputNumber = round(std::stod(str) * 1000000) / 1000000;
+
+    if (!calc.CheckDigitOnExpress(inputNumber))
+    {
+        std::cout << "Введенное число превышает границы заданных параметров." << std::endl;
+        return 0;
+    }
+
+    calc.Start(inputNumber);
+
+    system("pause");
+    return 0;
+}
